29d94daaf8423ef962b5654709cb7a4f73f70194
sharillas <ruperpost@hotmail.com>
2026-08-23 16:01:07 +0100

    Add header for CCCAM3 web interface

---
+++ src/api/cccam3_web_interface.h
@@ -0,0 +1,7 @@
+#ifndef CCCAM3_WEB_INTERFACE_H
+#define CCCAM3_WEB_INTERFACE_H
+
+// --- Função para servir a página web ---
+void cccam_web_interface_serve(int client_fd);
+
+#endif // CCCAM3_WEB_INTERFACE_H
