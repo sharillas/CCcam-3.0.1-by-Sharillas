bbb07cdd1b8eb51c4ef00a106d619c80e00b7b2b
sharillas <ruperpost@hotmail.com>
2026-08-23 16:23:32 +0100

    Add include for cccam3_handshake_advanced.h

--- include/cccam3.h
+++ include/cccam3.h
@@ -10,6 +10,7 @@
 #include "cccam3_web_interface.h"
 #include "cccam3_user_manager.h"
 #include "cccam3_crypto_advanced.h"
+#include "cccam3_handshake_advanced.h"
 
 #include <stdio.h>
 #include <stdlib.h>
