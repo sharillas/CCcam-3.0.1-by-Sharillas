4385f3310825bd619897d6793bde9f3a5c0f1fb1
sharillas <ruperpost@hotmail.com>
2026-08-23 15:11:39 +0100

    Refactor cccam3_server.c to improve initialization

--- src/core/cccam3_server.c
+++ src/core/cccam3_server.c
@@ -1,8 +1,9 @@
 #include "cccam3.h"
-#include "cccam3_protocol.h"
 #include "cccam3_logger.h"
-#include "cccam3_utils.h"
-
+#include "cccam3_protocol.h"
+#include "cccam3_cache.h"
+#include "cccam3_ecm.h"
+#include "cccam3_client.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,6 +15,7 @@
 
 static int g_server_fd = -1;
 static int g_running = 1;
+static cccam_config_t g_config;
 
 // Handler para sinais (CTRL+C, etc.)
 static void cccam_signal_handler(int sig) {
@@ -24,6 +26,10 @@ static void cccam_signal_handler(int sig) {
 
 // Inicialização do servidor
 int cccam3_init(cccam_config_t *config) {
+    if (config) {
+        g_config = *config;
+    }
+
     // Configurar handlers de sinais
     signal(SIGINT, cccam_signal_handler);
     signal(SIGTERM, cccam_signal_handler);
@@ -31,6 +37,16 @@ int cccam3_init(cccam_config_t *config) {
     // Inicializar sub-sistemas
     if (cccam_protocol_init() != 0) {
         cccam_log(LOG_ERROR, "Falha ao inicializar protocolo");
+        return -1;
+    }
+
+    if (cccam_cache_init() != 0) {
+        cccam_log(LOG_ERROR, "Falha ao inicializar cache");
+        return -1;
+    }
+
+    if (cccam_ecm_init() != 0) {
+        cccam_log(LOG_ERROR, "Falha ao inicializar ECM handler");
         return -1;
     }
 
@@ -51,18 +67,18 @@ int cccam3_init(cccam_config_t *config) {
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(config->listen_port);
+    addr.sin_port = htons(g_config.listen_port);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(g_server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        cccam_log(LOG_ERROR, "Falha ao bindar porta %d: %s", config->listen_port, strerror(errno));
+        cccam_log(LOG_ERROR, "Falha ao bindar porta %d: %s", g_config.listen_port, strerror(errno));
         close(g_server_fd);
         g_server_fd = -1;
         return -1;
     }
 
     // Iniciar escuta
-    if (listen(g_server_fd, config->max_clients) < 0) {
+    if (listen(g_server_fd, g_config.max_clients) < 0) {
         cccam_log(LOG_ERROR, "Falha ao iniciar escuta: %s", strerror(errno));
         close(g_server_fd);
         g_server_fd = -1;
@@ -70,7 +86,7 @@ int cccam3_init(cccam_config_t *config) {
     }
 
     cccam_log(LOG_INFO, "CCcam3 servidor iniciado na porta %d (max clientes: %d)", 
-              config->listen_port, config->max_clients);
+              g_config.listen_port, g_config.max_clients);
     return 0;
 }
 
@@ -84,6 +100,8 @@ int cccam3_run(void) {
     fd_set read_fds;
     int max_fd = g_server_fd;
 
+    cccam_log(LOG_INFO, "Servidor em execução...");
+    
     while (g_running) {
         FD_ZERO(&read_fds);
         FD_SET(g_server_fd, &read_fds);
@@ -121,6 +139,14 @@ int cccam3_run(void) {
             // add_client_to_list(new_client);
         }
 
+        // Limpar entradas expiradas da cache periodicamente
+        static time_t last_cache_clean = 0;
+        time_t now = time(NULL);
+        if (now - last_cache_clean > 30) { // A cada 30 segundos
+            cccam_cache_clean_expired();
+            last_cache_clean = now;
+        }
+
         // TODO: Processar dados dos clientes
     }
 
@@ -133,6 +159,64 @@ void cccam3_cleanup(void) {
         close(g_server_fd);
         g_server_fd = -1;
     }
+    cccam_cache_cleanup();
+    cccam_ecm_cleanup();
     cccam_protocol_cleanup();
     cccam_log(LOG_INFO, "CCcam3 encerrado");
+}
+
+// Função main
+int main(int argc, char *argv[]) {
+    char *config_file = "conf/cccam3.conf";
+    int opt;
+
+    // Processar argumentos da linha de comando
+    while ((opt = getopt(argc, argv, "c:hv")) != -1) {
+        switch (opt) {
+            case 'c':
+                config_file = optarg;
+                break;
+            case 'h':
+                printf("CCcam3 %s\n", CCCAM3_VERSION);
+                printf("Uso: %s [opções]\n", argv[0]);
+                printf("  -c <file>  Ficheiro de configuração\n");
+                printf("  -h         Mostrar esta ajuda\n");
+                printf("  -v         Mostrar versão\n");
+                return 0;
+            case 'v':
+                printf("CCcam3 versão %s\n", CCCAM3_VERSION);
+                return 0;
+            default:
+                fprintf(stderr, "Uso: %s -c <config_file>\n", argv[0]);
+                return 1;
+        }
+    }
+
+    // Carregar configuração
+    cccam_config_t config;
+    if (cccam_load_config(config_file, &config) != 0) {
+        fprintf(stderr, "Falha ao carregar configuração de %s\n", config_file);
+        return 1;
+    }
+
+    // Inicializar logger
+    cccam_log_init(config.log_file, config.log_level);
+
+    // Mostrar configuração
+    cccam_print_config(&config);
+
+    // Inicializar servidor
+    if (cccam3_init(&config) != 0) {
+        cccam_log(LOG_ERROR, "Falha ao inicializar servidor");
+        return 1;
+    }
+
+    // Executar servidor
+    int result = cccam3_run();
+
+    // Limpeza
+    cccam3_cleanup();
+    cccam_log_close();
+
+    return result;
 }
