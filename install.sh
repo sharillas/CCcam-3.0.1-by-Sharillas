2650c2c00b72c11177f52948ddb3272ac614e1d8
sharillas <ruperpost@hotmail.com>
2026-08-25 14:02:08 +0100

    Publicar binarios simples (sem tar) mantendo build estatica

--- install.sh
+++ install.sh
@@ -130,13 +130,12 @@ if [ "$FROM_SOURCE" -eq 1 ]; then
 else
     echo ">> A descarregar binário ($ARCH)..."
     mkdir -p "$BIN_DIR"
-    download "$RELEASE_URL/CCcam3-$ARCH.tar.gz" "/tmp/cccam3-$ARCH.tar.gz" || {
+    download "$RELEASE_URL/cccam3-$ARCH" "$BIN_DIR/cccam3.tmp" || {
         echo "ERRO: falha a descarregar o binário."
         exit 1
     }
-    tar xzf "/tmp/cccam3-$ARCH.tar.gz" -C /tmp
-    install -m 0755 /tmp/cccam3 "$BIN_DIR/cccam3"
-    rm -f "/tmp/cccam3-$ARCH.tar.gz" /tmp/cccam3
+    chmod 0755 "$BIN_DIR/cccam3.tmp"
+    mv -f "$BIN_DIR/cccam3.tmp" "$BIN_DIR/cccam3"
 fi
 
 echo ">> Binário instalado em $BIN_DIR/cccam3"
